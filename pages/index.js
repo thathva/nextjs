92da3fec5ffb0f85d94a740f63ba6725eda96d52
Yenuga, Thathva Sri Sai Reddy <tyenuga@iu.edu>
2023-03-27 11:01:30 -0400

    middleware

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,26 @@
-import { useState, useEffect } from "react";
+import { rickAndMortyMiddleware } from "./middleware";
 
-
-const Home = () => {
-  const [characters, setCharacters] = useState([]);
-
-  useEffect(() => {
-    fetch("https://rickandmortyapi.com/api/character")
-      .then((response) => response.json())
-      .then((data) => setCharacters(data.results))
-      .catch((error) => console.log(error));
-  }, []);
-
+const Home = ({ characters }) => {
   return (
-    <div className="container" style={{ 'max-width': '800px', 'margin': '0 auto' }}>
+    <div className="container" style={{ 'maxWidth': '800px', 'margin': '0 auto' }}>
       <h1>Characters</h1>
       <div className="grid" style={{ 'display': 'grid', 'grid-template-columns': 'repeat(auto-fit, minmax(200px, 1fr))', 'gap': '20px' }}>
         {characters.map((character) => (
           <div key={character.id} className="card" style={{
-            'background-color': '#fff',
+            'backgroundColor': '#fff',
             'padding': '20px',
-            'border-radius': '10px',
-            'box-shadow': '0 2px 4px rgba(0, 0, 0, 0.1)',
-            'text-align': 'center'
+            'borderRadius': '10px',
+            'boxShadow': '0 2px 4px rgba(0, 0, 0, 0.1)',
+            'textAlign': 'center'
           }}>
             <img src={character.image} alt={character.name} style={{
               'display': 'block',
               'width': '100%',
-              'border-radius': '5px',
-              'margin-bottom': '10px'
+              'borderRadius': '5px',
+              'marginBottom': '10px'
             }} />
             <h2 style={{
-              'font-size': '1.5rem',
+              'fontSize': '1.5rem',
               'margin': '0 0 10px'
             }}>{character.name}</h2>
             <p style={{ 'margin': '0' }}>Status: {character.status}</p>
@@ -41,6 +31,11 @@ const Home = () => {
       </div>
     </div>
   );
+};
+
+Home.getInitialProps = async () => {
+  const data = await rickAndMortyMiddleware();
+  return data;
 };
 
 export default Home;
