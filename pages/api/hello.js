7cd64d4e0a8cead11e90bb075be5ca7a0a423623
josephj1o4e1 <josephj1o4e1.goofygroove@gmail.com>
2023-04-11 04:06:12 -0400

    not looping, console.log debug

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -7,11 +7,13 @@ export default async function handler(req, res) {
   try {
     const characters = [];
     let url = 'https://rickandmortyapi.com/api/character'
+    console.log(url);
     do {
       const response = await fetch(url);
       const data = await response.json();
       url = data.next;
       characters.push(...data.results);
+      console.log(url);
     } while (url && data.pages<10);
 
     // const response = await fetch("https://rickandmortyapi.com/api/character"); // /?page=20
