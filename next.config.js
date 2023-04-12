cd6eaf7892aefc5c14f88d2932e3075aa055842b
josephj1o4e1 <josephj1o4e1.goofygroove@gmail.com>
2023-04-12 18:57:25 -0400

    next.config.js -> webpack ignore pg-native

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    config.plugins.push(new webpack.IgnorePlugin(/^pg-native$/));
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
