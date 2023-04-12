f671f657724cba3cd033b63e825e40c772272c5e
josephj1o4e1 <josephj1o4e1.goofygroove@gmail.com>
2023-04-12 19:07:35 -0400

    next.config.js -> webpack ignore pg-native...require('webpack') -> resourceRegExp: /^pg-native$/, contextRegExp

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const webpack = require('webpack');
+let resourceRegExp=/^pg-native$/
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config, options) => {
-    config.plugins.push([new webpack.IgnorePlugin(/^pg-native$/)]);
-
+    config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/, contextRegExp }));
+    
     return config;
   },
 }
