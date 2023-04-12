5ede926f699c52106af254335a80769cbb575a80
josephj1o4e1 <josephj1o4e1.goofygroove@gmail.com>
2023-04-12 19:03:15 -0400

    next.config.js -> webpack ignore pg-native...require('webpack') -> add brackets

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config, options) => {
-    config.plugins.push(new webpack.IgnorePlugin(/^pg-native$/));
+    config.plugins.push([new webpack.IgnorePlugin(/^pg-native$/)]);
 
     return config;
   },
