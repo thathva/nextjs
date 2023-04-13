0f65d8c5423fe4902b247a9a2627dd56f1d70f33
josephj1o4e1 <josephj1o4e1.goofygroove@gmail.com>
2023-04-13 05:30:05 -0400

    Same Prob: Trial and error2

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 const Home = () => {
   const [customers, setCustomers] = useState([]);
-  const resss=[];
+  let resss;
   // const [isLoading, setLoading] = useState(false)
   // const [characters, setCharacters] = useState([]);
   
