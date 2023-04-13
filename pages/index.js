3843aba3af29de4ce1e295ff3fc393be02de919d
josephj1o4e1 <josephj1o4e1.goofygroove@gmail.com>
2023-04-13 06:41:06 -0400

    trial n error 4

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 const Home = () => {
   const [customers, setCustomers] = useState([]);
-  // const [isLoading, setLoading] = useState(false)
+  const [isLoading, setLoading] = useState(false)
   // const [characters, setCharacters] = useState([]);
   
   useEffect(() => {
